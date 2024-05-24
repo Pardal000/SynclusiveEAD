db5941041a0d1b399c4b62534ef6959c5e776b15
Pardal000 <jorgepardal35@gmail.com>
2024-05-24 11:24:27 +0100

    git commit -m 'message'

---
+++ Ficha2/ex6.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Digita o primeiro numero: "))
+numero2 = int(input("Digita o segundo numero: "))
+
+if numero1+numero2 >= 100 :
+    print(numero1,"+",numero2, "é maior ou igual a 100")
+else :
+    print(numero1,"+",numero2, "não é maior que 100")
