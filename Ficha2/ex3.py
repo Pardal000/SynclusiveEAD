75c1598c03186970cd92a6478777fdfc4cee63ca
Pardal000 <jorgepardal35@gmail.com>
2024-05-24 11:23:58 +0100

    git commit -m 'message'

---
+++ Ficha2/ex3.py
@@ -0,0 +1,6 @@
+pi = 3.1415
+n = 200
+
+resultado = pi * n
+
+print("O valor da multiplicação de",pi,"por",n,"è",resultado)
---
+++ Ficha2/ex4.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Digita o primeiro numero: "))
+numero2 = int(input("Digita o segundo numero: "))
+
+print ("A soma é",numero1+numero2)
+print ("A subtração é",numero1-numero2)
+print ("A multiplicação  é",numero1*numero2)
+print ("A divisão  é",numero1/numero2)
